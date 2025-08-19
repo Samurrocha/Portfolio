167e79f41bccd5e8a88760ad474c2039ac8f7cc6
samurrocha <samuel.rrocha12@gmail.com>
2025-08-19 17:30:46 -0300

    adicionando o primeiro projeto

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,18 +1,35 @@
-import '../styles/components/projects.css'
+import Conversor from '../assets/go-planeje-photos/Conversor.png';
+import Home from '../assets/go-planeje-photos/Home.png';
+import Login from '../assets/go-planeje-photos/Login.png';
+import MyTravels from '../assets/go-planeje-photos/MyTravels.png';
+import Register from '../assets/go-planeje-photos/Register.png';
+import '../styles/components/projects.css';
+import CarrouselList from './carrousel_list';
 
 export default function Projects() {
 
+    const projectImages = [Conversor, Home, Login, MyTravels, Register];
+
     return (
-        
+
         <div className='projects' id='projects'>
             <h1>My Projects</h1>
             <div className='projectContainer'>
                 <div className='project'>
                     <div className='projectMedia'>
-                        a
+                        <CarrouselList projectsImages={projectImages} />
                     </div>
-                    <h2>Project 1</h2>
-                    <p>Description of project 1</p>
+                    <h2>Go-Planeje</h2>
+                    <p>
+                        O projeto é um sistema de gerenciamento de viagens que une uma interface simples e intuitiva a uma arquitetura robusta e escalável.
+                        Além de priorizar a experiência do usuário, foi desenvolvido com foco em boas práticas de engenharia de software, garantindo organização, manutenibilidade e flexibilidade para evoluções futuras.
+                    </p>
+                    <a href="https://go-planeje-frontend.vercel.app/" target="_blank">
+                        Link do projeto
+                    </a>
+                    <a href="https://github.com/Samurrocha/GO-PLANEJE-FRONTEND" target="_blank">
+                        Repositorio do Projeto
+                    </a>
                 </div>
                 <div className='project'>
                     <div className='projectMedia'>
--- src/components/carrousel_list.tsx
+++ src/components/carrousel_list.tsx
@@ -1,81 +1,40 @@
 import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { MdNavigateNext, MdNavigateBefore } from "react-icons/md"; // Material Design
 import '../styles/components/carrousel_list.css';
 
-export default function CarrouselList() {
+type CarrouselListProps = {
+  projectsImages: string[]; // recebe um array de imagens
+};
+
+export default function CarrouselList({ projectsImages }: CarrouselListProps) {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const projects = [
-    {
-      projectName: "SisTarefas",
-      projectDescription: "kdmkasdasdasdasdasdsadasdasd"
-
-    },
-
-    {
-      projectName: "RanchApp",
-      projectDescription: "saaaaaaaaaaaaaaaaaaaaaaaaaa"
-    },
-
-    {
-      projectName: "TransactonSys",
-      projectDescription: "saaaaaaaaaaaaaaaaaaaaaaaaaa"
-    }
-
-  ];
 
   const setNextProject = () => {
 
-    setCurrentIndex((currentIndex + 1) % projects.length)
-  }
-
-  const setBeforeProject = () => {
-
-    let beforeProject = (currentIndex - 1)
-
-    if (beforeProject < 0) {
-      beforeProject = projects.length - 1
-    }
-
-    setCurrentIndex(beforeProject)
+    setCurrentIndex((currentIndex + 1) % projectsImages.length)
   }
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % projects.length);
-    }, 10000);
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsImages.length);
+    }, 5000);
 
     return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
-  }, [projects.length]);
+  }, [projectsImages.length]);
 
   return (
-    <div id="presentationContainer">
-      <button onClick={setBeforeProject} className='passButton'>
-        <MdNavigateBefore size={40}/>
-      </button>
-      <div id='project'>
-        <AnimatePresence mode='wait'>
-          <motion.div
-            key={currentIndex}
-            initial={{ opacity: 0, x: 100 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -100 }}
-            transition={{ duration: 0.5 }}
-          >
-            <h2>
-              {projects[currentIndex].projectName}
-            </h2>
-
-            <div className='body'>
-              {projects[currentIndex].projectDescription}
-            </div>
-          </motion.div>
-        </AnimatePresence>
-      </div>
-
-      <button onClick={setNextProject} className='passButton'>
-        <MdNavigateNext size={35}/>
-      </button>
+    <div id="presentationContainer" onClick={setNextProject}>
+      <AnimatePresence mode='wait'>
+        <motion.div
+          key={currentIndex}
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+        >
+          <img src={projectsImages[currentIndex]} className='projectImage' />
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
