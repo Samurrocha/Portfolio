f0586bdbbe07062ae2de16253565bdaf5fa4d328
samurrocha <samuel.rrocha12@gmail.com>
2026-01-12 17:20:14 -0300

    fazendo merge de dev

--- src/components/contacts.tsx
+++ src/components/contacts.tsx
@@ -1,4 +1,4 @@
-import { FaEnvelope, FaLinkedin, FaGithub, FaPhone } from "react-icons/fa6"
+import { FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa6"
 import '../styles/components/contacts.css'
 
 
@@ -12,7 +12,6 @@ export default function Contacts() {
                 <a href="https://mail.google.com/mail/?view=cm&fs=1&to=samuel.rrocha12@gmail.com"><FaEnvelope size={20} color="white"/></a>
                 <a href="https://www.linkedin.com/in/samuel-rodrigues-lima/" target="_blank" rel="Linkedin"><FaLinkedin size={20} color="white"/></a>
                 <a href="http://github.com/samurrocha" target="_blank" rel="GitHub"> <FaGithub size={20} color="white"/> </a>
-                <a href="https://wa.me/5561994567819" target="_blank" rel="Phone"> <FaPhone size={20} color="white"/> </a>
             </div>
             
         </>
